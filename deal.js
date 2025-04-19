b0384722d9d369cf2aafcd89868f9af80cc8957e
Andrea Calderon <anncalderonn3gmail.com>
2025-04-18 21:15:26 -0700

    implemented gameState and stand.js functions and fixed utils errors.

--- deal.js
+++ deal.js
@@ -2,8 +2,8 @@
 //add count amount of cards to player's hand
 
 //dealCards(deck, playerHand, 2); // for starting hand
-export function dealCards(deck, hand, count){
-    for (i = 0; i < count; i++){
+export function dealCard(deck, hand, count){
+    for (let i = 0; i < count; i++){
         //first card in deck list gets appended to hand
         hand.push(deck.pop());
     }
