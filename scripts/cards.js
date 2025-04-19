bbc2137fe3787eeef5f2598069c273f2ccb5903d
Joshua Castaneda <j6castaneda@ucsd.edu>
2025-04-19 13:32:30 -0700

    Default Deck Creation

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,21 +1,22 @@
+
+
 //creating deck, suit and val
 
 /*representing : 0-51 
  Number % mod 13 => rank
 */
 
-const ranks = ["A", "2","3","4","5", "6", "7","8", "9","10","J","Q","K"];
-const suits = ["Spades", "Diamonds", "Clubs", "Hearts"];
-
-//creating blank deck 
-export function createDeck(){
+/**
+ * Creates a regular deck (1 - 52 unshuffled)
+ * @returns {Number[]}
+ */
+export function createDefaultDeck() {
     const deck = [];
-    for (let i = 0; i < 52; i++){
-        const rank = ranks[i % 13];
-        //each suit has 13 cards
-        const suit = suits[Math.floor(i/13)];
-        deck.push({rank,suit});
+
+    for (let i = 1; i <= 52; i++) {
+        deck.push(i);
     }
+
     return deck;
 }
 
